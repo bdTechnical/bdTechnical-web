bc5eea5c2d6ee68227cb3071fb0568fc31a55021
jdlr-static <justin.nn.dlr+netlify@gmail.com>
2022-04-11 17:33:12 +1000

    update home cms preview

--- src/js/cms-preview-templates/components/jumbotron.js
+++ src/js/cms-preview-templates/components/jumbotron.js
@@ -2,20 +2,17 @@ import React from "react";
 
 export default class Jumbotron extends React.Component {
   render() {
-    const {image, title, subtitle} = this.props;
+    const {title} = this.props;
     return <div>
       <div className="pv5 pv6-l ph3 bg-center cover" style={{
-        backgroundImage: image && `url(${image})`
+        backgroundColor: `var(--grey-4)`
       }}>
         <div className="mw7 center ph3">
           <div className="db mb3">
             <div className="mw7 relative bg-fix-primary mb3">
-              <h1 className="f2 f1-l b di lh-title mb3 white mw6 bg-primary">
-                { title }
+              <h1 className="f2 f1-l b di lh-title mb3 white mw6">
+                {entry.getIn(["data", "jumbotron", "title_pre"])} _____ {entry.getIn(["data", "jumbotron", "title_post"])}
               </h1>
-            </div>
-            <div className="mw7 relative bg-fix-primary">
-              {subtitle && <p className="b f4 di lh-title mb3 white mw6 bg-primary">{ subtitle }</p>}
             </div>
           </div>
         </div>
